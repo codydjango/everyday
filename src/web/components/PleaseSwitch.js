9bca6a068478295350aff7f6233f7120a4ae3b1a
Cody Django Redmond <codydjango@gmail.com>
2019-02-22 20:51:16 -0800

    fixed

--- src/web/components/PleaseSwitch.js
+++ src/web/components/PleaseSwitch.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default props => (
-    <div className="app instruction">
+    <div className="app messageScreen">
         <div className="message">
-            <span className="message-line">################################################################</span>
-            <span className="message-line">################# <strong>please switch to the mainnet</strong> #################</span>
-            <span className="message-line">################################################################</span>
+            <span className="messageLine">################################################################</span>
+            <span className="messageLine">################# <strong>please switch to the mainnet</strong> #################</span>
+            <span className="messageLine">################################################################</span>
         </div>
     </div>)
