cb9b8c9e71ed3ce347c619a04b4c6bb0c7ff7bc0
Cody Django Redmond <codydjango@gmail.com>
2019-03-02 14:28:37 -0800

    provide more info about web3 provider

--- src/web/index.js
+++ src/web/index.js
@@ -8,36 +8,15 @@ import web3Init from './web3init'
 import 'babel-polyfill'
 import './scss/index.scss'
 
-async function getNetworkType(web3) {
-    let networkType
-    try {
-        networkType = await web3.eth.net.getNetworkType()
-        console.log(`network type: ${ networkType }`)
-    } catch (err) {
-        console.log('can\'t detect web3 network type')
-        networkType = 'unknown'
-    }
-
-    return networkType
-}
-
 (async function init() {
     // figure out web3. we're gonna install our own and use an infura provider
     // if they don't have their own provider set up.
-    window.web3 = web3Init(window)
-
-    const web3Version = window.web3.version.api || window.web3.version
-    const isMetamask = window.web3.currentProvider.isMetaMask
-
-    console.log('web3 version', web3Version)
-    console.log('metamask', isMetamask)
+    const { providerType, networkType } = await web3Init(window)
 
     // if no metamask ask to install
-    if (!isMetamask) return render(<InstallMetamask />, document.getElementById('root'))
+    if (providerType !== "metamask") return render(<InstallMetamask />, document.getElementById('root'))
 
     // figure out network. If it's not on the mainnet give them a little prompt.
-    const networkType = await getNetworkType(window.web3)
-    console.log('networkType', networkType)
     if (networkType !== 'main') return render(<SwitchNetwork />, document.getElementById('root'))
 
     // all good, start the app
--- src/web/web3init.js
+++ src/web/web3init.js
@@ -1,12 +1,66 @@
 import Web3 from 'web3'
 
-export default function web3Init(window) {
-    if ((typeof window.ethereum !== 'undefined')
-    || (typeof window.web3 !== 'undefined')) {
-        return new Web3(window['ethereum'] || window.web3.currentProvider)
-    } else {
-        return new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/c63d2ec360ce413ea4dc8b10e0cf1fac"))
+function getProviderType(provider) {
+    if (provider.isMetaMask) {
+        return 'metamask'
+    } else if (provider.host && provider.host.indexOf('infura') !== -1) {
+        return 'infura'
+    } else if (provider.host && provider.host.indexOf('localhost') !== -1) {
+        return 'localhost'
     }
 
-    return null
+    return 'unknown'
+}
+
+async function getNetworkType(web3) {
+    let networkType
+
+    try {
+        networkType = await web3.eth.net.getNetworkType()
+    } catch (err) {
+        console.log('can\'t detect web3 network type')
+        networkType = 'unknown'
+    }
+
+    return networkType
+}
+
+export default async function web3Init() {
+    let provider, web3, enabled, defaultAccount
+
+    // https://metamask.github.io/metamask-docs/Advanced_Concepts/Provider_API
+    if (typeof window.ethereum !== 'undefined'
+        || (typeof window.web3 !== 'undefined')) {
+
+          // Web3 browser user detected. You can now use the provider.
+        provider = window['ethereum'] || window.web3.currentProvider
+    } else {
+        provider = new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/c63d2ec360ce413ea4dc8b10e0cf1fac")
+    }
+
+    window.web3 = web3 = new Web3(provider)
+
+    if (window.ethereum) {
+        enabled = await window.ethereum.enable()
+        defaultAccount = enabled[0]
+        console.log(`web3 enabled with default account: ${ defaultAccount }`)
+    }
+
+    const web3Version = web3.version.api || web3.version
+    console.log(`web3 version: ${ web3Version }`)
+    const providerType = getProviderType(provider)
+    console.log(`web3 provider type: ${ providerType }`)
+    const isConnected = (providerType === "metamask") ? provider.isConnected() : provider.connected
+    console.log(`web3 provider connected: ${ isConnected }`)
+    const networkType = await getNetworkType(web3)
+    console.log(`web3 connected network: ${ networkType }`)
+
+    return {
+        web3,
+        web3Version,
+        providerType,
+        isConnected,
+        networkType,
+        defaultAccount
+    }
 }
