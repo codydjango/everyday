2db59a0a0a002ac8d946d19d8b5cf47ae47ca3b2
Cody Django Redmond <codydjango@gmail.com>
2019-03-15 17:08:29 -0700

    add context

--- src/web/context/index.js
+++ src/web/context/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const ListContext = React.createContext()
 const NotesContext = React.createContext()
-const AuthenticationContext = React.createContext()
+const AuthContext = React.createContext()
 const StatusContext = React.createContext()
 
 export {
     ListContext,
     NotesContext,
-    AuthenticationContext,
+    AuthContext,
     StatusContext
 }
